51faf535ea1a00d9412166f3e4cc9fc99179168e
cullendales <cullendales@gmail.com>
2025-08-03 05:58:43 +0700

    Ignore C# build artifacts

--- scenes/server.gd
+++ scenes/server.gd
@@ -1,3 +1,10 @@
 extends Node3D
 
-const port : int = 7777
+var client := StreamPeerTCP.new()
+
+func _ready():
+	var err := client.connect_to_host("127.0.0.1", 7777)
+	if err == OK:
+		print("Connected to server!")
+	else:
+		print("Failed to connect: ", err)
